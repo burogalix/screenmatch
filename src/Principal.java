6c0c8f7409d9687d6da1f329c5d2ddfbfcd92017
backjunior <burogalix@gmail.com>
2024-05-20 16:48:08 -0300

    metodo avaliacao e media

--- src/Principal.java
+++ src/Principal.java
@@ -7,6 +7,11 @@ public class Principal {
         meuFilme.duracaoEmMinutos = 180;
 
         meuFilme.exibeFichaTecnica();
-
-        }
+        meuFilme.avalia(8);
+        meuFilme.avalia(6);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
     }
+}
