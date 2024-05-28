53aea867052345438996f7cdb268f1308babeb09
backjunior <burogalix@gmail.com>
2024-05-28 16:11:14 -0300

    extends

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -13,5 +14,10 @@ public class Principal {
         meuFilme.avalia(10);
         System.out.println("Total de avaliações: "+meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(7);
     }
 }
